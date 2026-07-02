3f9eefc693e11838a1bc2475f6fec42dd40b25ce
MThor <Keinov88@yandex.ru>
2026-07-02 19:30:50 +0300

    Updates

---
+++ icons/index.ts
@@ -0,0 +1,5 @@
+export { default as ColdWaterIcon } from './ColdWater';
+export { default as HotWaterIcon } from './HotWater';
+export { default as LightningIcon } from './Lightning';
+export { default as FlameIcon } from './Flame';
+export { default as BasketIcon } from './Basket';
